d534a07d427679f8d7dda43301bbf9396e69b15c
morispolanco <morispolanco@gmail.com>
2023-11-10 00:57:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const preguntaInput = document.getElementById("pregunta-input");
-  const claveApiInput = document.getElementById("clave-api-input");
-  const obtenerRespuestaButton = document.getElementById(
-    "obtener-respuesta-button"
-  );
+import React, { useState } from 'react';
+import axios from 'axios';
 
-  obtenerRespuestaButton.addEventListener("click", function () {
-    const pregunta = preguntaInput.value;
-    const claveApi = claveApiInput.value;
+function Buscador() {
+    const [pregunta, setPregunta] = useState('');
+    const [respuesta, setRespuesta] = useState('');
 
-    if (!pregunta) {
-      alert("Por favor, escriba una pregunta o caso.");
-      return;
-    }
-
-    if (!claveApi) {
-      alert("Por favor, ingrese una clave de API de Respell.");
-      return;
-    }
-
-    axios
-      .post(
-        "https://api.respell.ai/v1/run",
-        {
-          spellId: "k0GhQkJOn7IKEY-BdghY6",
-          inputs: {
-            pregunta: pregunta
-          }
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${claveApi}`,
-            "Content-Type": "application/json"
-          }
+    const handleSubmit = async () => {
+        if (!pregunta) {
+            alert('Por favor, escriba una pregunta o caso.');
+            return;
         }
-      )
-      .then(function (response) {
-        const respuesta =
-          response.data.outputs.respuesta || "No se pudo obtener una respuesta";
-        alert("Respuesta: " + respuesta);
-      })
-      .catch(function (error) {
-        alert("Error al enviar la solicitud a la API");
-      });
-  });
-});
+
+        try {
+            const api_key = 'YOUR_API_KEY'; // Reemplaza con tu API key
+            const response = await axios.post(
+                'https://api.respell.ai/v1/run',
+                {
+                    spellId: 'k0GhQkJOn7IKEY-BdghY6',
+                    inputs: {
+                        pregunta: pregunta
+                    }
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${api_key}`,
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+
+            if (response.status === 200) {
+                setRespuesta(response.data.outputs.respuesta);
+            } else {
+                alert('Error al enviar la solicitud a la API');
+            }
+        } catch (error) {
+            alert('Error en la solicitud');
+        }
+    };
+
+    return (
+        <div>
+            <h1>Buscador</h1>
+            <p>Esta aplicación responde preguntas sobre la legislación de Guatemala, buscando en la Web.</p>
+            <p>Por Moris Polanco</p>
+
+            <textarea value={pregunta} onChange={(e) => setPregunta(e.target.value)} placeholder="Pregunta o caso"></textarea>
+            <button onClick={handleSubmit}>Obtener Respuesta</button>
+
+            {respuesta && <div><strong>Respuesta:</strong> {respuesta}</div>}
+        </div>
+    );
+}
+
+export default Buscador;
