c0697620ed3563da3a2b39fd5b22cfeb3b6d8e7c
morispolanco <morispolanco@gmail.com>
2023-11-16 18:34:35 -0600

    Update script.js

--- secretario/script.js
+++ secretario/script.js
@@ -1,66 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+const generateButton = document.getElementById('generate-button');
+const inputTextArea = document.getElementById('input-textarea');
+const outputText = document.getElementById('output-text');
 
-public class SpellApiExample {
-
-    public static void main(String[] args) throws IOException {
-        // URL de la API
-        URL url = new URL("https://api.respell.ai/v1/run");
+generateButton.addEventListener('click', function() {
+  const text = inputTextArea.value;
+  
+  if (text.trim() !== '') {
+    const requestData = {
+      spellId: "qPnyGRPqmYt7xjSLRX8t_",
+      spellVersionId: "ulAKJd58ZYWMorqDCBSy_",
+      inputs: {
+        input: text
+      }
+    };
     
-        // Crea una conexión
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    
-        // Establece el método POST
-        connection.setRequestMethod("POST");
-    
-        // Establece los encabezados de la solicitud
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setRequestProperty("Authorization", "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805");
-        connection.setRequestProperty("Content-Type", "application/json");
-    
-        // Habilita el envío y recepción de datos
-        connection.setDoOutput(true);
-        connection.setDoInput(true);
-    
-        // Crea el cuerpo de la solicitud
-        String requestBody = "{\n" +
-                "  \"spellId\": \"qPnyGRPqmYt7xjSLRX8t_\",\n" +
-                "  \"spellVersionId\": \"ulAKJd58ZYWMorqDCBSy_\",\n" +
-                "  \"inputs\": {\n" +
-                "    \"input\": \"Example text\"\n" +
-                "  }\n" +
-                "}";
-    
-        // Envía la solicitud
-        OutputStream outputStream = connection.getOutputStream();
-        outputStream.write(requestBody.getBytes());
-        outputStream.flush();
-    
-        // Obtiene la respuesta
-        int responseCode = connection.getResponseCode();
-    
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-    
-            while ((inputLine = bufferedReader.readLine()) != null) {
-                response.append(inputLine);
-            }
-    
-            bufferedReader.close();
-    
-            // Procesa la respuesta JSON
-            System.out.println(response.toString());
-        } else {
-            System.out.println("Error en la solicitud. Código de respuesta: " + responseCode);
-        }
-    
-        // Cierra la conexión
-        connection.disconnect();
-    }
-}
+    fetch('https://api.respell.ai/v1/run', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      const output = data.outputs.output;
+      outputText.textContent = output;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+});
